f33639aa71781ec76160114d800b6094332e22f3
Jonah Kirangi <jonahkirangi@gmail.com>
2014-02-01 23:51:31 -0800

    Update spec file

--- test/date_spec.rb
+++ test/date_spec.rb
@@ -3,7 +3,7 @@ require "hash"
 
 describe "Verify application is running" do
   it "takes date input and outputs day of the week" do
-    caclulate = HashedDate.new
-    caclulate.range(2003, 3, 3).must_equal 'Mo'
+    calculate = HashedDate.new
+    calculate.range(2003, 3, 3).must_equal 'Mo'
   end
 end
